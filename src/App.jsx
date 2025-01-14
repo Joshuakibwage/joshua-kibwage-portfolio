af4185323c478b5cf063f0376806d2136a3dd6c3
Josh <joshuakibwage23@gmail.com>
2025-01-14 08:30:23 +0300

    Add reveal page and portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Portfolio from "./components/Portfolio"
+import Skills from "./components/Skills"
 
 
 function App() {
 
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <Hero />
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </div>
   )
 }
 
